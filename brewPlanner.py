fbe7c78c4ee628948c75d808d1a62051218273eb
Mick <cartemic@oregonstate.edu>
2017-02-18 10:44:57 -0800

    began python document for calculations

---
+++ brewPlanner.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 10:37:56 2017
+
+@author: Mick
+"""
+
+# %%  BREW PLANNER
+# under construction
+# for planning and documenting homebrews
+
+
+# %% IMPORT PACKAGES
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# %% DEFINE TERMS
+# GOAL: build a GUI for these inputs
+
+
+# %% DEFINE FUNCTIONS
+
+
+# %% USE FUNCTIONS
+# GOAL: build a gui for this as well
+
